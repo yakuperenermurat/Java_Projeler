825488527948e6eec8478d0d05d33044961ebf55
Yakup eren ermurat <yakuperenermurat@gmail.com>
2024-04-23 20:20:16 +0300

    aritmetik İşlemlera
    aritmetik İşlemlera

---
+++ JavaAlistirmalari/src/week1/AritmetikIslemlerVeIslemOncelikleri.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.util.Scanner;
+
+public class AritmetikIslemlerVeIslemOncelikleri {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        // Kullanici sayilara giris bolumu
+        System.out.print("a sayisini giriniz: ");
+        int a = scanner.nextInt();
+        System.out.print("b sayisini giriniz: ");
+        int b = scanner.nextInt();
+        System.out.print("c sayisini giriniz: ");
+        int c = scanner.nextInt();
+
+        //Sorulan islemin yapıldigi yer
+        int sonuc = a + b * c - b;
+        System.out.println("sonuc = " + sonuc);
+    }
+}
