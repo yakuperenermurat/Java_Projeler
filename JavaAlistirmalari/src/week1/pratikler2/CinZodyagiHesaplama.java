e1a8d2613d4fd2e4715cf2eaafdd53ba4d994ed7
Yakup eren ermurat <yakuperenermurat@gmail.com>
2024-04-23 18:33:40 +0300

    Çin zordayağı hesaplama
    Çin zordayağı hesaplama

---
+++ JavaAlistirmalari/src/week1/pratikler2/CinZodyagiHesaplama.java
@@ -0,0 +1,42 @@
+package week1.pratikler2;
+
+import java.util.Scanner;
+
+public class CinZodyagiHesaplama {
+
+    public static void main(String[] args) {
+
+        String burc = "";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        int dogumYili = input.nextInt();
+
+        if (dogumYili % 12 == 0) {
+            burc = "Maymun";
+        } else if (dogumYili % 12 == 1) {
+            burc = "Horoz";
+        } else if (dogumYili % 12 == 2) {
+            burc = "Köpek";
+        } else if (dogumYili % 12 == 3) {
+            burc = "Domuz";
+        } else if (dogumYili % 12 == 4) {
+            burc = "Fare";
+        } else if (dogumYili % 12 == 5) {
+            burc = "Öküz";
+        } else if (dogumYili % 12 == 6) {
+            burc = "Kaplan";
+        } else if (dogumYili % 12 == 7) {
+            burc = "Tavşan";
+        } else if (dogumYili % 12 == 8) {
+            burc = " Ejderha";
+        } else if (dogumYili % 12 == 9) {
+            burc = "Yılan";
+        } else if (dogumYili % 12 == 10) {
+            burc = "At";
+        } else if (dogumYili % 12 == 11) {
+            burc = "Koyun";
+        }
+        System.out.println("Çin Zodyağı Burcunuz : " + burc);
+    }
+}
+
