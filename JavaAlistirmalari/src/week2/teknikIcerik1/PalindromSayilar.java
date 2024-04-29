9303bd76ede7224140d21f104f917b03a2bbe400
Yakup eren ermurat <yakuperenermurat@gmail.com>
2024-04-29 14:24:36 +0300

    palindrom sayilar

---
+++ JavaAlistirmalari/src/week2/teknikIcerik1/PalindromSayilar.java
@@ -0,0 +1,22 @@
+package week2.teknikIcerik1;
+
+public class PalindromSayilar {
+    static boolean isPalindrom(int number) {
+        int temp = number , reverseNumber = 0 ,lastNumber  ;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10 ) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String args[]) {
+
+        System.out.println(isPalindrom(4));
+
+
+    }
+}
