82ab12d3de7ead1d9dd5f9db0ab85ffd37191365
yakuperenermurat <yakuperenermurat@gmail.com>
2024-05-02 22:06:54 +0300

    salary calculator change

--- JavaAlistirmalari/src/week2/teknikIcerik3/salaryCalculator/salaryCalculator.java
+++ JavaAlistirmalari/src/week2/teknikIcerik3/salaryCalculator/salaryCalculator.java
@@ -1,14 +1,14 @@
 package week2.teknikIcerik3.salaryCalculator;
 
-import java.util.Calendar;
-
 public class salaryCalculator {
 
     // Degiskenler tanimlandi.
     private String name;
     private double salary;
     private int workHours;
-    private int hireYear = 2021;
+    private int hireYear;
+    double raisePercentage; // maas yüzdesi
+    double raiseAmount;
 
     //Kurucu fonksiyon tanimlanmasi
     public salaryCalculator(String name, double salary, int workHours, int hireYear) {
@@ -34,12 +34,10 @@ public class salaryCalculator {
         }
     }
     //Maas artisi hesaplama
-    public double raiseSalary() {
+    public void raiseSalary() {
         int yearsWorked = 2021 - hireYear;
-        System.out.println(yearsWorked);
-        double raisePercentage; // maas yüzdesi
 
-        if (yearsWorked < 10) {
+        if (yearsWorked >= 0 && yearsWorked < 10) {
             raisePercentage = 0.05;
         } else if (yearsWorked < 20) {
             raisePercentage = 0.1;
@@ -47,14 +45,12 @@ public class salaryCalculator {
             raisePercentage = 0.15;
         }
 
-        double raiseAmount = salary * raisePercentage; //Maas artim miktari
-        return raiseAmount;
+        raiseAmount = salary * raisePercentage; //Maas artim miktari
     }
 
     //Bilgilerin ekrana yazdirilmasi
     @Override
     public String toString() {
-        double raiseAmount = raiseSalary();
         double totalSalary = salary + raiseAmount - tax() + bonus(); //Zamlı maas
         return "Adı: " + name + "\n" +
                 "Maaşı: " + salary + "\n" +
@@ -69,6 +65,7 @@ public class salaryCalculator {
 
     public static void main(String[] args) {
         salaryCalculator employee = new salaryCalculator("kemal", 2000.0, 45, 1985);
+        employee.raiseSalary();
         System.out.println(employee);
     }
 }
